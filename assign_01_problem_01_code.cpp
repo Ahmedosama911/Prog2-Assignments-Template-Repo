a1168740074f9d1d1d1f53763d214ed9a8583fbd
Ahmedosama911 <130244297+Ahmedosama911@users.noreply.github.com>
2023-04-08 21:57:58 +0200

    Add files via upload

---
+++ assign_01_problem_01_code.cpp
@@ -0,0 +1,15 @@
+//Ahmed osama fathy
+//202106438
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << "Hello, " << str;
+    return 0;
+}
