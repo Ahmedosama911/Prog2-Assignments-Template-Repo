cb7962dda2378d10997f6864814658ff83c1e26c
Ahmedosama911 <130244297+Ahmedosama911@users.noreply.github.com>
2023-04-30 21:49:49 +0200

    Add files via upload

---
+++ Assign_02_problem_02_code.java
@@ -0,0 +1,18 @@
+//ahmed osama fathy
+//202106438
+
+
+import java.util.Scanner;
+
+public class lap {
+    public static void main(String[]args){
+        Scanner sc=new Scanner (System.in);
+       int num=sc.nextInt();
+       for(int i=1;i<=12;i++){
+           int sum=i*num;
+           System.out.println(num+" * "+i+" = "+sum);
+       }
+           
+         
+    }
+}
